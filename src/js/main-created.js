65ea7aaa7d7725fa8419dd0261ba3556c4d39e6b
YevheniiaBondarenko <65921369+YevheniiaBondarenko@users.noreply.github.com>
2020-12-13 22:56:01 +0200

    skrollLeft

--- src/js/main-created.js
+++ src/js/main-created.js
@@ -1,21 +1,49 @@
-import selesTpl from '../templates/sales-section.hbs';
-import propertyTpl from '../templates/propetry-section.hbs';
-import transportTpl from '../templates/transport-section.hbs';
-import workTpl from '../templates/work-section.hbs';
-import electronicsTpl from '../templates/electronics-section.hbs';
-import recreationAndSportTpl from '../templates/recreationAndSport-section.hbs';
-import freeTpl from '../templates/sales-section.hbs';
-import businessAndServicesTpl from '../templates/businessAndServices-section.hbs';
-import tradeTpl from '../templates/trade-section.hbs';
+const Handlebars = require('handlebars');
 
+import sectionTpl from '../templates/section.hbs';
+
+const templateNames = {
+  businessAndServices: 'Бізнес та послуги',
+  electronics: 'Електроніка',
+  free: 'Віддам безкоштовно',
+  sales: 'Розпродаж',
+  propetry: 'Нерухомість',
+  recreationAndSport: 'Відпочинок і спорт',
+  trade: 'Обмін',
+  transport: 'Транспорт',
+  work: 'Робота',
+};
+
+// section-name скрыть хедер по этому класу после нажатия дивитись всы
+//  заменить класс card-field на card-field-wrap
 const refs = {
   logoEL: document.querySelector('.js-logo-open'),
   mainContainerEL: document.querySelector('.js-render-main-page'),
+  btnLeftEL: document.querySelector('.swipe-left'),
+  btnRightEL: document.querySelector('.swipe-right'),
 };
 console.log(refs.logoEL);
 console.log(refs.mainContainerEL);
 
 refs.logoEL.addEventListener('click', getCard);
+
+let left = 0;
+let right = 0;
+
+function getLeft(id) {
+  return function (e) {
+    const listCardEL = document.querySelector(`#${id} .card-field`);
+    const CardEL = document.querySelector(`#${id} .card-item`);
+    console.log('left');
+    console.log(CardEL.offsetWidth);
+    left = left - CardEL.offsetWidth - 25;
+    if (left < -listCardEL.offsetWidth) {
+      e.preventDefault();
+    }
+    listCardEL.style.left = left + 'px';
+  };
+}
+function getRight() {}
 
 const API = 'https://callboard-backend.herokuapp.com/call?page=';
 let page = 1;
@@ -36,37 +64,32 @@ function getCard(e) {
   console.log('Клик был, функция запущена');
 
   e.preventDefault();
+  clearArticlesContainer();
 
   postData(API + page).then(data => {
     console.log(data);
-    const sales = data.sales || [];
-    createCategoryMarkup(sales, selesTpl);
-    const property = data.property || [];
-    createCategoryMarkup(property, propertyTpl);
-    const transport = data.transport || [];
-    createCategoryMarkup(transport, transportTpl);
-    const work = data.work || [];
-    createCategoryMarkup(work, workTpl);
-    const electronics = data.electronics || [];
-    createCategoryMarkup(electronics, electronicsTpl);
-    const recreationAndSport = data.recreationAndSport || [];
-    createCategoryMarkup(recreationAndSport, recreationAndSportTpl);
-    const businessAndServices = data.businessAndServices || [];
-    createCategoryMarkup(businessAndServices, businessAndServicesTpl);
-    const free = data.free || [];
-    createCategoryMarkup(free, freeTpl);
-    const trade = data.trade || [];
-    createCategoryMarkup(trade, tradeTpl);
+    Object.keys(data).forEach(key => {
+      createCategoryMarkup(data[key], key);
+      const btnLeftEL = document.querySelector(`#${key} .swipe-left`);
+      btnLeftEL.addEventListener('click', getLeft(key));
+    });
+
+    console.log(CardEL.offsetWidth);
+    console.log(listCardEL.offsetWidth);
   });
 }
 
-function createCategoryMarkup(arg, tpl) {
+function createCategoryMarkup(arg, key) {
+  console.log(templateNames[key]);
   if (arg.length === 0) {
     return;
   }
-  refs.mainContainerEL.insertAdjacentHTML('beforeend', tpl(arg));
+  refs.mainContainerEL.insertAdjacentHTML(
+    'beforeend',
+    sectionTpl({ data: arg, name: templateNames[key], id: key }),
+  );
 }
 
-// function clearArticlesContainer() {
-//   refs.articlesContainer.innerHTML = '';
-// }
+function clearArticlesContainer() {
+  refs.mainContainerEL.innerHTML = '';
+}
