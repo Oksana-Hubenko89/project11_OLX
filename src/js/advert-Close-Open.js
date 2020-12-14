b6ce5eb70786e71b858ea12e8fd447065276a4a2
BelikSergey <65306747+BelikSergey@users.noreply.github.com>
2020-12-14 20:45:48 +0200

    попытка слить свой код c ветки belik9

--- src/js/advert-Close-Open.js
+++ src/js/advert-Close-Open.js
@@ -1,10 +1,14 @@
 const refsAdvert={
-    // cardAdvert: document.querySelector('.js-advert'),
-    openModal: document.querySelector('.js-advert-open'),
-    advertCloseModal: document.querySelector('.js-advert-close'),
-  
-  }
-console.log(refsAdvert.cardAdvert);
+  advertTitle: document.querySelector('.js-advert-title'),
+  advertPrice: document.querySelector('.js-advert-price'),
+  openModal: document.querySelector('.js-advert-open'),
+  advertCloseModal: document.querySelector('.js-advert-close'),
+  advertIMG: document.querySelector('.js-advert-img'),
+  sellerName: document.querySelector('.js-seller-name'),
+  sellerTime: document.querySelector('.js-seller-time'),
+  sellerTel: document.querySelector('.js-seller-tel'),
+}
+console.log(refsAdvert.advertTitle.textContent);
 // console.log(refsAdvert.openModal);
 // console.log(refsAdvert.advertCloseModal);
 
@@ -13,7 +17,7 @@ document.addEventListener('click', addModalAdvert)
 
 // // закрытие модалки через кнопку
 refsAdvert.advertCloseModal.addEventListener('click', modalClose);
- 
+
 // закрытие модалки по Esc
 document.addEventListener('keydown', modalEscClose);
 
@@ -22,27 +26,65 @@ refsAdvert.openModal.addEventListener('click', onModalBackdropClick);
 
 // Функция открытия модалки
 function addModalAdvert(event) {
-  let target = event.target;
-  // console.log(target.parentNode.classList[0]);
-    if (target.parentNode.classList[0] ==='card-item') {
+let target = event.target;
+// console.dir(target.parentElement.children[1].textContent);
+  if (target.parentElement.classList[0] ==='card-item') {
+    // обнуление картинки
+    refsAdvert.advertIMG.src = '/';
 
-      refsAdvert.openModal.classList.remove('is-hidden');
-    }
+    changeImgOnAdvert(target);
+     // подставлять значения цены 
+    changeAdvertTitle(target);
+    
+    // открыть модалку
+    refsAdvert.openModal.classList.remove('is-hidden');
+    
+   
+   
+    // console.dir(target.parentElement);
+    // рендерить и подставлять фотки под основной картинкой.
+
+  }
 }
+
+// Функции закрытия модалки
+function modalClose() {
+  refsAdvert.openModal.classList.add('is-hidden');
+}
+function modalEscClose(evt) {
+  if (evt.key === "Escape") {
+    modalClose();
+  }
+}
+function onModalBackdropClick(evt) {
+  // console.dir(evt.target.attributes.class.value);
+  // console.log(evt.target.dataset.action);
+  if (evt.target.attributes.class.value.includes("add-backdrop")){
+    modalClose();
+  }
+} 
+// функция получения и подставления адруса картинки 
+function changeImgOnAdvert(target) {
+  // Брать значение картинки и переподставлять в свою в модалке
+  refsAdvert.advertIMG.src = target.parentElement.firstElementChild.firstElementChild.currentSrc;
+}
+function changeAdvertTitle(target) {
+  let item = target.parentElement.children;
   
-  // Функции закрытия модалки
-  function modalClose() {
-    refsAdvert.openModal.classList.add('is-hidden');
+  // refsAdvert.advertTitle.textContent='';
+
+  console.log(item[1].textContent);
+  console.log(refsAdvert.advertTitle.textContent);
+  refsAdvert.advertPrice.textContent='';
+  
+  refsAdvert.advertTitle.textContent = item[1].textContent;
+
+  if(item[2].children.length === 2 &&
+     item[2].children[1].textContent !=="" ){
+      //  console.log(item[2].children[1].textContent);
+    refsAdvert.advertPrice.textContent = item[2].children[1].textContent;
+  } else {
+    refsAdvert.advertPrice.textContent= item[2].children[0].textContent;
   }
-  function modalEscClose(evt) {
-    if (evt.key === "Escape") {
-      modalClose();
-    }
-  }
-  function onModalBackdropClick(evt) {
-    console.dir(evt.target.attributes.class.value);
-    // console.log(evt.target.dataset.action);
-    if (evt.target.attributes.class.value.includes("add-backdrop")){
-      modalClose();
-    }
-  } 
+    
+}
--- src/js/advert-logic.js
+++ src/js/advert-logic.js
@@ -1,5 +1,16 @@
-// const refs ={
-//     btnInfoSeller: document.querySelector('.js-info-seller'),
+const refs ={
+    btnInfoSeller: document.querySelector('.js-info-seller'),
+    info: document.querySelector('.js-info'),
+    infoSeller: document.querySelector('.js-seller-on'),
+    
+}
+// console.dir(refs.btnInfoSeller);
 
-// }
-// // console.log(refs.btnInfoSeller);
+refs.btnInfoSeller.addEventListener('click', showSellerInfo);
+
+function showSellerInfo(params) {
+    refs.info.classList.toggle('visually-hidden');
+    refs.btnInfoSeller.classList.toggle('seller');
+    refs.infoSeller.classList.toggle('visually-hidden');
+
+}
