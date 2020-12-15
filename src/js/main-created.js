3a38505ea74b573d1c02ce778674ceced5eaefee
YevheniiaBondarenko <65921369+YevheniiaBondarenko@users.noreply.github.com>
2020-12-15 22:11:46 +0200

    Доделала

--- src/js/main-created.js
+++ src/js/main-created.js
@@ -2,6 +2,9 @@ const Handlebars = require('handlebars');
 
 import sectionTpl from '../templates/section.hbs';
 
+const API = 'https://callboard-backend.herokuapp.com/call?page=';
+let page = 1;
+
 const templateNames = {
   businessAndServices: 'Бізнес та послуги',
   electronics: 'Електроніка',
@@ -20,29 +23,22 @@ const refs = {
   mainContainerEL: document.querySelector('.js-render-main-page'),
   headerMenuEL: document.querySelector('.header-menu'),
   paginatorPagesEL: document.querySelector('.pagination-conteiner'),
-  // itemMenuEL: document.querySelectorAll('.site-nav-item'),
 };
-console.log(refs.headerMenuEL);
 
 refs.headerMenuEL.addEventListener('click', createCategoryMenu);
+refs.logoEL.addEventListener('click', onLogoCLick);
+refs.btnClearEL.addEventListener('click', onClearBtnClick);
+refs.paginatorPagesEL.addEventListener('click', changePage);
 
-function createCategoryMenu(e) {
-  e.preventDefault();
+async function createCategoryMenu(e) {
   const keyCategory = e.target.dataset.id;
   console.log(keyCategory);
-  getCard();
-  showAll(keyCategory);
+  await getCard(page, keyCategory);
+  showAll(keyCategory)();
 }
 
-const API = 'https://callboard-backend.herokuapp.com/call?page=';
-let page = 1;
-
-refs.logoEL.addEventListener('click', getCard);
-refs.btnClearEL.addEventListener('click', getCard);
-refs.paginatorPagesEL.addEventListener('click', chengPages);
-
-async function postData(url = '') {
-  const response = await fetch(url, {
+async function postData(page) {
+  const response = await fetch(API + page, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
@@ -53,44 +49,38 @@ async function postData(url = '') {
   return await response.json();
 }
 
-function chengPages(e) {
-  e.preventDefault();
-  page = e.target.dataset.id;
-  console.log(page);
-  getCard();
-}
-
-function getLeft(id, position, arg) {
-  return function (e) {
-    e.preventDefault();
-    const listCardEL = document.querySelector(`#${id} .card-field`);
-    const CardEL = document.querySelector(`#${id} .card-item`);
-    position.left = position.left + arg * (CardEL.offsetWidth + 25);
-    if (position.left === -listCardEL.offsetWidth || position.left === listCardEL.offsetWidth) {
-      e.preventDefault();
-    }
-    listCardEL.style.left = position.left + 'px';
-  };
-}
-
-function getCard(e) {
+async function getCard(page, selectedCategory = '') {
   console.log('Клик был, функция запущена');
-
-  e.preventDefault();
   clearArticlesContainer();
 
-  postData(API + page).then(data => {
+  await postData(page).then(data => {
     console.log(data);
-    Object.keys(data).forEach(key => {
-      createCategoryMarkup(data[key], key);
-      const btnLeftEL = document.querySelector(`#${key} .swipe-left`);
-      const btnRightEL = document.querySelector(`#${key} .swipe-right`);
-      const showAllEL = document.querySelector(`#${key} .show-all`);
-      const position = { left: 0 };
-      btnLeftEL.addEventListener('click', getLeft(key, position, +1));
-      btnRightEL.addEventListener('click', getLeft(key, position, -1));
-      showAllEL.addEventListener('click', showAll(key));
-    });
+
+    const partialData = filterData(data, selectedCategory);
+
+    renderMainContent(partialData);
+  });
+}
+
+function filterData(data, selectedCategory) {
+  if (selectedCategory) {
+    return {
+      [selectedCategory]: data[selectedCategory],
+    };
+  }
+  return data;
+}
+
+function renderMainContent(data) {
+  Object.keys(data).forEach(key => {
+    createCategoryMarkup(data[key], key);
+    const btnLeftEL = document.querySelector(`#${key} .swipe-left`);
+    const btnRightEL = document.querySelector(`#${key} .swipe-right`);
+    const showAllEL = document.querySelector(`#${key} .show-all`);
+    const position = { left: 0 };
+    btnLeftEL.addEventListener('click', getLeft(key, position, +1));
+    btnRightEL.addEventListener('click', getLeft(key, position, -1));
+    showAllEL.addEventListener('click', showAll(key));
   });
 }
 
@@ -109,6 +99,28 @@ function showAll(id) {
   };
 }
 
+function changePage(e) {
+  page = e.target.dataset.id;
+  console.log(refs.paginatorPagesEL.children);
+  const oldEl = document.querySelector(`.is-active`);
+  oldEl.classList.remove('.is-active');
+  e.target.classList.add('is-active');
+  getCard(page);
+}
+
+function getLeft(id, position, arg) {
+  return function (e) {
+    e.preventDefault();
+    const listCardEL = document.querySelector(`#${id} .card-field`);
+    const CardEL = document.querySelector(`#${id} .card-item`);
+    position.left = position.left + arg * (CardEL.offsetWidth + 25);
+    if (position.left === -listCardEL.offsetWidth || position.left === listCardEL.offsetWidth) {
+      e.preventDefault();
+    }
+    listCardEL.style.left = position.left + 'px';
+  };
+}
+
 function createCategoryMarkup(arg, key) {
   if (arg.length === 0) {
     return;
@@ -121,4 +133,12 @@ function createCategoryMarkup(arg, key) {
 
 function clearArticlesContainer() {
   refs.mainContainerEL.innerHTML = '';
+}
+
+function onLogoCLick(e) {
+  getCard(page);
+}
+
+function onClearBtnClick(e) {
+  getCard(page);
 }
