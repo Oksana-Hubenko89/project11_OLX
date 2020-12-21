f34b6a8c25867a19d8a2e396efd6d28abbecef81
Katerina Kraieva <katika207@gmail.com>
2020-12-21 10:53:32 +0200

    qwerty

--- src/js/add-modal-post.js
+++ src/js/add-modal-post.js
@@ -25,16 +25,15 @@ function addFormSend(evt) {
     if (addModalError === 0) {
       let formData = new FormData(addForm);
       let config = {
-      method: 'POST',
-      url: `${BASE_URL}/call`,
-      headers: {
-      'accept': 'application/json',
-      'Content-Type': 'multipart/form-data',
-    //   'Authorizatioin': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiI1ZmMyMDg1YmQwOTM2NTI4MTA3Y2UyNzQiLCJzaWQiOiI1ZmMyZDJmY2UxZDIwNTA2NzAyYmRkMjIiLCJpYXQiOjE2MDY2MDM1MTYsImV4cCI6MTYwNjYwNzExNn0.rJ_QjU4KvA76H96RHsvOBChK0Vjbd0NmqjMxdQVJIXA'
-      'Authorization': `${accessToken}`,
-       },
-      data: formData,
-    };
+        method: 'POST',
+        url: 'https://callboard-backend.herokuapp.com/call',
+        headers: {
+          'accept': 'application/json', 
+          'Content-Type': 'multipart/form-data',
+          'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiI1ZmQzMzYxZjgwZGFiZDAwMTc5ZDdmZjYiLCJzaWQiOiI1ZmRlNTllMjY0NTAxYjAwMTczMDhmMmIiLCJpYXQiOjE2MDg0MDc1MjIsImV4cCI6MTYwODQxMTEyMn0.3byp5ioPGXyUIjbv867SFgWSoQoevmOeshTuJzZ6JLg'
+        },
+        data: formData,
+      };
         axios(config)
         .then(function (response) {
           console.log(response.data);
