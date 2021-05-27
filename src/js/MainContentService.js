c65ced17f34860f11fef5e96e160395db6e3b34e
Hubenko Oksana <17zov89@gmail.com>
2021-05-27 14:53:04 +0300

    исправление отображении ссылки первой страницы

--- src/js/MainContentService.js
+++ src/js/MainContentService.js
@@ -1,17 +1,17 @@
-const LOAD_CONTENT_URL = 'https://callboard-backend.herokuapp.com/call?page=';
+// const LOAD_CONTENT_URL = 'https://callboard-backend.herokuapp.com/call?page=';
 
-export default class Service {
+// export default class Service {
 
-  async loadMainContent( ) {
-    let page= 1;
-    const response = await fetch(LOAD_CONTENT_URL + page, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      redirect: 'follow',
-      referrerPolicy: 'no-referrer',
-    });
-    return await response.json();
-  }
-}
+//   async loadMainContent( ) {
+//     let page= 1;
+//     const response = await fetch(LOAD_CONTENT_URL + page, {
+//       method: 'GET',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       redirect: 'follow',
+//       referrerPolicy: 'no-referrer',
+//     });
+//     return await response.json();
+//   }
+// }
